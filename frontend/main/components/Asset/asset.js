4cd33c74e4307e555923a712745f86d6209470ff
JoseJaime Figueroa <jaimefigueroa84@gmail.com>
2019-08-05 11:53:35 -0600

    Create componenet Asset

--- frontend/main/components/Asset/asset.js
+++ frontend/main/components/Asset/asset.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { withRouter } from 'next/router';
-import { NoSsr, Typography, Grid } from '@material-ui/core';
+import { NoSsr, Typography, Grid, TextField } from '@material-ui/core';
 import FormContainer from '../../core/FormContainer';
+import { InputBase } from '@material-ui/core';
 
 import AssetService from './asset.service';
+///start:slot:dependencies<<<///end:slot:dependencies<<<
 
 const service = new AssetService();
 const defaultConfig = {
   service
+  ///start:slot:config<<<///end:slot:config<<<
 };
 
 class AssetForm extends FormContainer {
@@ -18,30 +21,40 @@ class AssetForm extends FormContainer {
 
   componentDidMount() {
     console.log('Form did mount');
-    this.load();
+    this.load(this.props.data.Id);
+
+    ///start:slot:didMount<<<///end:slot:didMount<<<
   }
 
   AFTER_LOAD = entity => {
     console.log('AFTER_LOAD', entity);
+    ///start:slot:afterLoad<<<///end:slot:afterLoad<<<
   };
 
   AFTER_CREATE = instance => {
     console.log('AFTER_CREATE', instance);
+
+    ///start:slot:afterCreate<<<///end:slot:afterCreate<<<
   };
 
   AFTER_CREATE_AND_CHECKOUT = entity => {
     console.log('AFTER_CREATE_AND_CHECKOUT', entity);
+    ///start:slot:afterCreateCheckout<<<///end:slot:afterCreateCheckout<<<
   };
 
   AFTER_SAVE = entity => {
     console.log('AFTER_SAVE', entity);
     const { dialog } = this.props;
     if (dialog) dialog.close('ok');
+    ///start:slot:afterSave<<<///end:slot:afterSave<<<
   };
 
   BEFORE_CHECKIN = () => {
     console.log('BEFORE_CHECKIN');
+    ///start:slot:beforeCheckin<<<///end:slot:beforeCheckin<<<
   };
+
+  ///start:slot:js<<<///end:slot:js<<<
 
   render() {
     const { dialog } = this.props;
@@ -51,9 +64,78 @@ class AssetForm extends FormContainer {
 
     return (
       <NoSsr>
-        <Typography variant='h3' className='' gutterBottom>
-          This is the [asset] component.
-        </Typography>
+        <Grid className='' container direction='column' item xs={12}>
+          <TextField
+            type='text'
+            label='CPU-Name'
+            value={baseEntity.CPUName || ''}
+            onChange={event => this.handleInputChange(event, 'CPUName')}
+            style={{ textAlign: 'left' }}
+            margin='normal'
+            disabled={this.isDisabled}
+            fullWidth
+          />
+          <TextField
+            type='text'
+            label='Model'
+            value={baseEntity.Model || ''}
+            onChange={event => this.handleInputChange(event, 'Model')}
+            style={{ textAlign: 'left' }}
+            margin='normal'
+            disabled={this.isDisabled}
+            fullWidth
+          />
+          <TextField
+            type='text'
+            label='Serial Number'
+            value={baseEntity.SerialNumber || ''}
+            onChange={event => this.handleInputChange(event, 'SerialNumber')}
+            style={{ textAlign: 'left' }}
+            margin='normal'
+            disabled={this.isDisabled}
+            fullWidth
+          />
+          <TextField
+            type='text'
+            label='Ram'
+            value={baseEntity.Ram || ''}
+            onChange={event => this.handleInputChange(event, 'Ram')}
+            style={{ textAlign: 'left' }}
+            margin='normal'
+            disabled={this.isDisabled}
+            fullWidth
+          />
+          <TextField
+            type='text'
+            label='CPU'
+            value={baseEntity.CPU || ''}
+            onChange={event => this.handleInputChange(event, 'CPU')}
+            style={{ textAlign: 'left' }}
+            margin='normal'
+            disabled={this.isDisabled}
+            fullWidth
+          />
+          <TextField
+            type='text'
+            label='Location'
+            value={baseEntity.Location || ''}
+            onChange={event => this.handleInputChange(event, 'Location')}
+            style={{ textAlign: 'left' }}
+            margin='normal'
+            disabled={this.isDisabled}
+            fullWidth
+          />
+          <TextField
+            type='text'
+            label='Usuario'
+            value={baseEntity.Usuario || ''}
+            onChange={event => this.handleInputChange(event, 'Usuario')}
+            style={{ textAlign: 'left' }}
+            margin='normal'
+            disabled={this.isDisabled}
+            fullWidth
+          />
+        </Grid>
       </NoSsr>
     );
   }
--- frontend/main/components/Asset/assets.js
+++ frontend/main/components/Asset/assets.js
@@ -4,12 +4,25 @@ import { NoSsr, Typography, Grid } from '@material-ui/core';
 import SearchBox from '../../widgets/Searchbox';
 import Pagination from 'react-js-pagination';
 import ListContainer from '../../core/ListContainer';
+import { Table } from '@material-ui/core';
+import { TableHead } from '@material-ui/core';
+import { TableBody } from '@material-ui/core';
+import { TableRow } from '@material-ui/core';
+import { TableCell } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { Icon } from '@material-ui/core';
+import { InputBase } from '@material-ui/core';
+import Dialog from '../../widgets/Dialog';
+import Asset from './asset.js';
+import { AppBar, Toolbar } from '@material-ui/core';
 
 import AssetService from './asset.service';
+///start:slot:dependencies<<<///end:slot:dependencies<<<
 
 const service = new AssetService();
 const defaultConfig = {
   service
+  ///start:slot:config<<<///end:slot:config<<<
 };
 
 class AssetsList extends ListContainer {
@@ -21,36 +34,221 @@ class AssetsList extends ListContainer {
   componentDidMount() {
     console.log('List did mount');
     this.load();
+
+    ///start:slot:didMount<<<///end:slot:didMount<<<
   }
 
   AFTER_LOAD = () => {
     console.log('AFTER_LOAD');
+    ///start:slot:afterLoad<<<///end:slot:afterLoad<<<
   };
 
   AFTER_CREATE = instance => {
     console.log('AFTER_CREATE', instance);
+
+    ///start:slot:afterCreate<<<
+    this.openDialog(instance);
+    ///end:slot:afterCreate<<<
   };
 
   AFTER_CREATE_AND_CHECKOUT = entity => {
     console.log('AFTER_CREATE_AND_CHECKOUT', entity);
+    ///start:slot:afterCreateCheckout<<<///end:slot:afterCreateCheckout<<<
   };
 
   AFTER_REMOVE = () => {
     console.log('AFTER_REMOVE');
+    ///start:slot:afterRemove<<<///end:slot:afterRemove<<<
   };
 
   ON_OPEN_ITEM = item => {
     console.log('ON_OPEN_ITEM', item);
+
+    ///start:slot:onOpenItem<<<
+    this.openDialog(item);
+    ///end:slot:onOpenItem<<<
   };
+
+  openDialog = item => {
+    this.setState({
+      asset: item
+    });
+  };
+
+  closeDialog = feedback => {
+    if (feedback == 'ok') {
+      this.refresh();
+    }
+    this.setState({
+      asset: false
+    });
+  };
+  ///start:slot:js<<<///end:slot:js<<<
 
   render() {
     const { isLoading, baseEntity, baseList } = this.state;
 
     return (
       <NoSsr>
-        <Typography variant='h3' className='' gutterBottom>
-          This is the [assets] component.
+        <Grid className='container-fluid' container direction='column' item xs={12} style={{ padding: 20 }} />
+        <Typography variant='h5' className='h5' gutterBottom>
+          Assets
         </Typography>
+        <Grid container direction='row'>
+          <Grid item xs />
+          <Pagination
+            activePage={this.state.filterOptions.page}
+            itemsCountPerPage={this.state.filterOptions.limit}
+            totalItemsCount={this.state.filterOptions.totalItems}
+            pageRangeDisplayed={5}
+            onChange={newPage => {
+              this.pageChanged(newPage);
+            }}
+          />
+        </Grid>
+        <Table className='' size='small'>
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>CPU-Name</TableCell>
+              <TableCell>Model</TableCell>
+              <TableCell>Serial Number</TableCell>
+              <TableCell>Ram</TableCell>
+              <TableCell>CPU</TableCell>
+              <TableCell>Location</TableCell>
+              <TableCell>Usuario</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {baseList &&
+              baseList.map(item => (
+                <TableRow key={item.Id}>
+                  <TableCell>
+                    <Grid container direction='row' className='row' justify='center' alignItems='flex-start' spacing={2}>
+                      <Grid item xs>
+                        <Button
+                          variant='contained'
+                          color='default'
+                          className=''
+                          onClick={event => {
+                            this.openItem(event, item);
+                          }}
+                          size='small'
+                        >
+                          <Icon>edit</Icon>Open
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </TableCell>
+                  <TableCell>
+                    <InputBase
+                      type='text'
+                      className=''
+                      autoComplete='off'
+                      disabled={this.isDisabled}
+                      readOnly={true}
+                      onChange={event => this.handleInputChange(event, 'CPUName')}
+                      value={item.CPUName || ''}
+                      fullWidth
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <InputBase
+                      type='text'
+                      className=''
+                      autoComplete='off'
+                      disabled={this.isDisabled}
+                      readOnly={true}
+                      onChange={event => this.handleInputChange(event, 'Model')}
+                      value={item.Model || ''}
+                      fullWidth
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <InputBase
+                      type='text'
+                      className=''
+                      autoComplete='off'
+                      disabled={this.isDisabled}
+                      readOnly={true}
+                      onChange={event => this.handleInputChange(event, 'SerialNumber')}
+                      value={item.SerialNumber || ''}
+                      fullWidth
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <InputBase
+                      type='text'
+                      className=''
+                      autoComplete='off'
+                      disabled={this.isDisabled}
+                      readOnly={true}
+                      onChange={event => this.handleInputChange(event, 'Ram')}
+                      value={item.Ram || ''}
+                      fullWidth
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <InputBase
+                      type='text'
+                      className=''
+                      autoComplete='off'
+                      disabled={this.isDisabled}
+                      readOnly={true}
+                      onChange={event => this.handleInputChange(event, 'CPU')}
+                      value={item.CPU || ''}
+                      fullWidth
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <InputBase
+                      type='text'
+                      className=''
+                      autoComplete='off'
+                      disabled={this.isDisabled}
+                      readOnly={true}
+                      onChange={event => this.handleInputChange(event, 'Location')}
+                      value={item.Location || ''}
+                      fullWidth
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <InputBase
+                      type='text'
+                      className=''
+                      autoComplete='off'
+                      disabled={this.isDisabled}
+                      readOnly={true}
+                      onChange={event => this.handleInputChange(event, 'Usuario')}
+                      value={item.Usuario || ''}
+                      fullWidth
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+        <Dialog open={!!this.state.asset} onClose={this.closeDialog} draggable title='Defect' okLabel='Save'>
+          {dialog => {
+            return !isLoading && <Asset dialog={dialog} data={this.state.asset} />;
+          }}
+        </Dialog>
+        <AppBar position='fixed' style={{ top: 'auto', bottom: 0 }}>
+          <Toolbar variant='dense'>
+            <SearchBox bindFilterInput={this.bindFilterInput} value={this.state.filterOptions.filterGeneral} />
+            <Grid item xs />
+            <Button
+              variant='contained'
+              color='default'
+              className=''
+              onClick={event => {
+                this.createInstance(event, {});
+              }}
+            >
+              <Icon>add_circle</Icon>New
+            </Button>
+          </Toolbar>
+        </AppBar>
       </NoSsr>
     );
   }
